0cb5984563eacab9656c1544f9574451cfebf7ef
Isla <Islajiin@gmail.com>
2025-05-15 10:57:24 +0900

    Feat: add BucketView view

---
+++ study/presentations/views/Bucket/BucketView.swift
@@ -0,0 +1,13 @@
+//  Copyright © 2025 ADA 4th Challenge3 Team1. All rights reserved.
+
+import SwiftUI
+
+struct BucketView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BucketView()
+}
