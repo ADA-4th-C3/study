3c15bd042f6e429a8e56011eef362e996aa978a8
Isla <Islajiin@gmail.com>
2025-05-16 09:54:18 +0900

    Feat: add BucketView view

--- study/presentations/views/Bucket/BucketView.swift
+++ study/presentations/views/Bucket/BucketView.swift
@@ -3,11 +3,11 @@
 import SwiftUI
 
 struct BucketView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
 }
 
 #Preview {
-    BucketView()
+  BucketView()
 }
