9db0b1ff04e026ef517030c90ddc8193dcb83fb4
Isla <Islajiin@gmail.com>
2025-05-17 08:33:14 +0900

    Feat: define BucketViewState

--- study/presentations/views/Bucket/BucketViewState.swift
+++ study/presentations/views/Bucket/BucketViewState.swift
@@ -1,13 +1,14 @@
 //  Copyright © 2025 ADA 4th Challenge3 Team1. All rights reserved.
 
-import SwiftUI
-
-struct BucketViewState: View {
-  var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct BucketViewState {
+  let bucketTitle : String
+  let bucketList : [Bucket]
+  
+  func copy(bucketTitle : String? = nil, bucketList: [Bucket]? = nil)->BucketViewState{
+    return BucketViewState(
+      bucketTitle: bucketTitle ?? self.bucketTitle,
+      bucketList: bucketList ?? self.bucketList
+    )
   }
 }
 
-#Preview {
-  BucketViewState()
-}
