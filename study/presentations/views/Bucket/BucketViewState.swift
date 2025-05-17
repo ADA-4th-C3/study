b64cdaa7e8683c3bf87d6a98a47d99abb02fdca0
Isla <Islajiin@gmail.com>
2025-05-17 10:16:04 +0900

    Feat: define BucketViewState with copy function

--- study/presentations/views/Bucket/BucketViewState.swift
+++ study/presentations/views/Bucket/BucketViewState.swift
@@ -1,10 +1,10 @@
 //  Copyright © 2025 ADA 4th Challenge3 Team1. All rights reserved.
 
 struct BucketViewState {
-  let bucketTitle : String
-  let bucketList : [Bucket]
-  
-  func copy(bucketTitle : String? = nil, bucketList: [Bucket]? = nil)->BucketViewState{
+  let bucketTitle: String
+  let bucketList: [Bucket]
+
+  func copy(bucketTitle: String? = nil, bucketList: [Bucket]? = nil) -> BucketViewState {
     return BucketViewState(
       bucketTitle: bucketTitle ?? self.bucketTitle,
       bucketList: bucketList ?? self.bucketList
