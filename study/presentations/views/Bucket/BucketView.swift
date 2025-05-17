e80662b3c676f8f0d6665ca71bd063cdb54d5405
Isla <Islajiin@gmail.com>
2025-05-18 06:44:25 +0900

    Feat: add bucket input, list display, and delete action

--- study/presentations/views/Bucket/BucketView.swift
+++ study/presentations/views/Bucket/BucketView.swift
@@ -6,12 +6,54 @@ struct BucketView: View {
   var body: some View {
     BaseView(
       create: { BucketViewModel() }
-    ) { _, _ in
-      VStack(spacing: 0) {}
+    ) { viewModel, state in
+      VStack(spacing: 0) {
+        // MARK: Toolbar
+        Toolbar(title: "Bucket")
+        
+        // MARK: TextField
+        HStack {
+          TextField("Write diary title", text: Binding(
+            get: { state.bucketTitle },
+            set: viewModel.setBucketTitle
+          ))
+          .textFieldStyle(RoundedBorderTextFieldStyle())
+          
+          IconButton("plus") {
+            viewModel.add()
+          }
+        }
+        .padding(.leading, 16)
+        Divider()
+        
+        if state.bucketList.isEmpty {
+          // MARK: Empty
+          Spacer()
+          Text("No Bucket")
+            .foregroundStyle(.gray)
+          
+        } else {
+          // MARK: Bucket List
+          
+          ForEach(state.bucketList, id: \.id) { bucket in
+            HStack(spacing:0){
+              Text(bucket.title)
+              Spacer()
+              IconButton("trash") {
+                viewModel.delete(bucket)
+              }
+            }
+            .padding(.leading, 16)
+          }
+        }
+        Spacer()
+      }
     }
   }
 }
 
 #Preview {
-  BucketView()
+  BasePreview {
+    BucketView()
+  }
 }
